b2f1aee16e15baa626c4b872e68ae56edd2da81e
Douglas Ross <ross@adobe.com>
2016-06-29 00:29:33 -0700

    Updating to include CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var cors = require('cors');
 var express = require('express');
 var app = express();
 
@@ -19,6 +20,7 @@ if (program.dir) {
       : process.cwd() + "/" + program.dir;
 }
 
+app.use(cors());
 app.use(express.static(staticDir));
 app.listen(port, function() {
   console.log("Static file server has started on port "
