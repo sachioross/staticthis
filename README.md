da3e7bf6497d5b67efe4976ef0795142b5d29989
Douglas Ross <ross@adobe.com>
2016-06-29 00:16:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Static This
+
+This is a very simple node module that is installed globally and can be used to quickly start a static-web server for any directory. 
+
+Simple use-cases that come to mind: 
+
+- Rapid preview of an in-development website
+- Show web files, such as JS and CSS, directly in the web-browser without needing to show the file:// protocol with the complete path to that file
+- View technical documentation that may be in HTML format, such as javadocs
+
+### Installation and usage
+```
+npm install -g 
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var express = require('express');
+var app = express();
+
+program
+  .version('0.0.1')
+  .option('-p, --port <n>', 'Port to run server')
+  .option('-d, --dir [dir]', 'Directory to run static server from, default is directory from which this process is executed')
+  .parse(process.argv);
+
+var port = program.port || 9100;
+var staticDir = process.cwd();
+
+if (program.dir) {
+  staticDir = program.dir.startsWith("/")
+      ? program.dir
+      : process.cwd() + "/" + program.dir;
+}
+
+app.use(express.static(staticDir));
+app.listen(port, function() {
+  console.log("Static file server has started on port "
+      + port
+      + " and is serving files from "
+      + staticDir);
+});
